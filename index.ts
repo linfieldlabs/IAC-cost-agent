d717404175fc506b2f9b86b59787f1288e3315c8
lakshith <lakshith.k.nishshanke@gmail.com>
2025-02-09 00:49:45 +0530

    create pr comments by workflow

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log("Hello, world!")
+import * as core from "@actions/core"
+import { context } from "@actions/github"
+import run from "./scripts/terraform-estimator"
+
+try {
+    const githubToken = process.env.GITHUB_TOKEN
+    const openaiApiKey = process.env.OPENAI_API_KEY
+
+    const prNumber = context?.payload?.pull_request?.number
+    const { owner, repo } = context?.repo
+
+    if (!githubToken) {
+        throw new Error("GitHub token not found")
+    }
+
+    if (!openaiApiKey) {
+        throw new Error("OpenAI API key not found")
+    }
+
+    if (!prNumber) {
+        throw new Error("Pull Request number is not found")
+    }
+
+    run(githubToken, openaiApiKey, repo, owner, prNumber)
+} catch (error) {
+    core.setFailed((error as Error).message)
+}
