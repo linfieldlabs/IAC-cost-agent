8fb901ac11ee296aaca0d978bccd26f620ab5fed
lakshith <lakshith.k.nishshanke@gmail.com>
2025-02-10 20:25:27 +0530

    add aws credentials to sample tf

--- test/data/sample.tf
+++ test/data/sample.tf
@@ -1,9 +1,31 @@
 provider "aws" {
   region = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-test-bucket"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      },
+    ]
+  })
 }
 
 resource "aws_lambda_function" "example" {
